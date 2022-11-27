ffac2a15813f0c111582b432acd11f37c53ff943
MagicStephen <50861510+MagicStephen@users.noreply.github.com>
2022-11-27 17:31:44 +0100

    Delete Projekt-BreakOut directory

--- Projekt-BreakOut/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.projektbreakout {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.projektbreakout to javafx.fxml;
-    exports com.example.projektbreakout;
-}
